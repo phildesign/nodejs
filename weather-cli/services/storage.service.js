287bdc82b1961d63bb9e5077e8850f28a34f853c
Phil <phil.nice.23@gmail.com>
2024-08-18 19:32:47 +0300

    feat: OS and path

---
+++ weather-cli/services/storage.service.js
@@ -0,0 +1,8 @@
+import { homedir } from 'os';
+import { join } from 'path';
+
+const filePath = join(homedir(), 'weather-data.json');
+
+const saveKeyValue = (key, value) => {};
+
+export { saveKeyValue };
