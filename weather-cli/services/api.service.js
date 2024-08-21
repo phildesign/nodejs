150c16ba524f1b5cf7eec9f09e4ce4199aebf897
Phil <phil.nice.23@gmail.com>
2024-08-21 04:44:12 +0300

    feat: Error handling

--- weather-cli/services/api.service.js
+++ weather-cli/services/api.service.js
@@ -1,4 +1,3 @@
-import https from 'https';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 import axios from 'axios';
 
@@ -17,26 +16,7 @@ const getWeather = async (city) => {
 		},
 	});
 
-	console.log(data);
-
 	return data;
-
-	// const url = new URL('https://api.openweathermap.org/data/2.5/weather');
-	// url.searchParams.append('q', city);
-	// url.searchParams.append('appid', token);
-	// url.searchParams.append('lang', 'ru');
-	// url.searchParams.append('units', 'metric');
-
-	// https.get(url, (response) => {
-	// 	let res = '';
-	// 	response.on('data', (chunk) => {
-	// 		res += chunk;
-	// 	});
-
-	// 	response.on('end', () => {
-	// 		console.log(res);
-	// 	});
-	// });
 };
 
 export { getWeather };
--- weather-cli/weather.js
+++ weather-cli/weather.js
@@ -17,19 +17,32 @@ const saveToken = async (token) => {
 	}
 };
 
+const getForcast = async () => {
+	try {
+		const weather = await getWeather(process.env.CITY);
+		console.log(weather);
+	} catch (error) {
+		if (error?.response?.status == 404) {
+			printError('Неверно указан город');
+		} else if (error?.response?.status == 401) {
+			printError('Неверно указан токен');
+		} else {
+			printError(error.message);
+		}
+	}
+};
+
 const initCLI = () => {
 	const args = getArgs(process.argv);
 	if (args.h) {
 		printHelp();
 	}
 	if (args.s) {
-		// Сохранить город
 	}
 	if (args.t) {
 		return saveToken(args.t);
 	}
-	getWeather('london');
-	// Вывести погоду
+	getForcast();
 };
 
 initCLI();
