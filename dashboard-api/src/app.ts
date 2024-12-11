7c7fc35e0dd738377611ad2c440d97af852a88f8
Phil <phil.nice.23@gmail.com>
2024-12-11 16:18:30 +0300

    feat: understanding DI and IOC

--- dashboard-api/src/app.ts
+++ dashboard-api/src/app.ts
@@ -1,22 +1,18 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
-import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/users.controller';
 import { ExeptionFilter } from './errors/exeption.filter';
+import { ILogger } from './logger/logger.interface';
 
 export class App {
 	app: Express;
 	server: Server;
 	port: number;
-	logger: LoggerService;
+	logger: ILogger;
 	userController: UserController;
 	exeptionFilter: ExeptionFilter;
 
-	constructor(
-		logger: LoggerService,
-		userController: UserController,
-		exeptionFilter: ExeptionFilter,
-	) {
+	constructor(logger: ILogger, userController: UserController, exeptionFilter: ExeptionFilter) {
 		this.app = express();
 		this.port = 8000;
 		this.logger = logger;
