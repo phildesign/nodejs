ae8389ca881e2a0301ef47e096ea3b1ad3de65d8
Phil <phil.nice.23@gmail.com>
2025-01-03 13:47:39 +0300

    feat: add Service users

--- dashboard-api/src/main.ts
+++ dashboard-api/src/main.ts
@@ -6,6 +6,9 @@ import { UserController } from './users/users.controller';
 import { TYPES } from './types';
 import { ILogger } from './logger/logger.interface';
 import { IExeptionFilter } from './errors/exeption.filter.interface';
+import { IUserService } from './users/users.service.interface';
+import { IUserController } from './users/users.controller.interface';
+import { UserService } from './users/users.service';
 
 export interface IBootsrapReturn {
 	appContainer: Container;
@@ -15,7 +18,8 @@ export interface IBootsrapReturn {
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<ILogger>(TYPES.ILogger).to(LoggerService);
 	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
-	bind<UserController>(TYPES.UserController).to(UserController);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUserService>(TYPES.UserService).to(UserService);
 	bind<App>(TYPES.Application).to(App);
 });
 
