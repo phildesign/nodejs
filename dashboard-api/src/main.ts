34b267ace128c7ab91f917e8c2fb2dafe81ebe79
Phil <phil.nice.23@gmail.com>
2025-01-04 16:02:27 +0300

    feat: add Configuration service

--- dashboard-api/src/main.ts
+++ dashboard-api/src/main.ts
@@ -9,6 +9,8 @@ import { IExeptionFilter } from './errors/exeption.filter.interface';
 import { IUserService } from './users/users.service.interface';
 import { IUserController } from './users/users.controller.interface';
 import { UserService } from './users/users.service';
+import { IConfigService } from './config/config.service.interface';
+import { ConfigService } from './config/config.service';
 
 export interface IBootsrapReturn {
 	appContainer: Container;
@@ -16,10 +18,11 @@ export interface IBootsrapReturn {
 }
 
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope();
 	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
 	bind<IUserController>(TYPES.UserController).to(UserController);
 	bind<IUserService>(TYPES.UserService).to(UserService);
+	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
 	bind<App>(TYPES.Application).to(App);
 });
 
